6a5c76058116624d9df9941b0ec81d75c0558c31
Andre Sarokhanian <andres1983@icloud.com>
2019-08-21 23:07:17 -0500

    added saving images locally

---
+++ ProceduralNoiseTerrain/FileWriteSave/SaveImageLocally.swift
@@ -0,0 +1,16 @@
+//
+//  SaveImageLocally.swift
+//  ProceduralNoiseTerrain
+//
+//  Created by Andre Sarokhanian on 8/21/19.
+//  Copyright © 2019 Andre Sarokhanian. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+
+enum StorageType {
+    case userDefaults
+    case fileSystem
+}
--- ProceduralNoiseTerrain/ViewController.swift
+++ ProceduralNoiseTerrain/ViewController.swift
@@ -10,48 +10,79 @@ import UIKit
 import SpriteKit
 
 class ViewController: UIViewController, StoryboardBound {
-
     
     @IBOutlet weak var skView: SKView!
-   
+    
+    
+    var savedImage: UIImage?
     
     let spriteNode = SKSpriteNode()
+    
     weak var coordinator: MainCoordinator?
-
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+       
         setup2DVC()
-      
+        
     }
-
+    
     @IBAction func generate(_ sender: Any) {
         let terrain = TerrainGenerator.generateTerrain(squared: 256)
-      
-        spriteNode.size = CGSize(width: 300, height: 300)
-      
-        
+        spriteNode.size = CGSize(width: 250, height: 250)
         spriteNode.texture = SKTexture(cgImage: terrain.cgImage())
         spriteNode.texture?.filteringMode = .nearest
     }
+    
+    
+   
+    
+    
     @IBAction func save(_ sender: Any) {
+        
+        
     }
     
 }
+
+
+
+extension ViewController {
+    
+    @objc func saveImage() {
+        
+        if let buildingImage = UIImage(named: "planet") {
+           
+            DispatchQueue.global(qos: .background).async {
+                self.store(image: buildingImage, forKey: "wroteimage", storageType: .fileSystem)
+            }
+        
+        }
+    }
+    
+    @objc func display() {
+        
+        DispatchQueue.global(qos: .background).async {
+            
+            if let savedImage = self.retrieveImage(forKey: "wroteimage", storageType: .fileSystem) {
+                
+                DispatchQueue.main.async {
+                    ///self.savedImageDisplayImageView.image = savedImage
+                }
+            }
+        
+        }
+    }
+}
+
 
 extension ViewController {
     
     
     func setup2DVC() {
-        
-        
-        
         let scene = SKScene(size: skView.bounds.size)
         scene.backgroundColor = UIColor.black
-        
-    
-        
         
         let particle = SKEmitterNode(fileNamed: "stars.sks")
         particle?.position = CGPoint(x: 1000, y: 0)
@@ -62,20 +93,11 @@ extension ViewController {
         spriteNode.position = CGPoint(x: skView.bounds.size.width/2, y: skView.bounds.size.height/2)
         spriteNode.zPosition = 2
         
-        
         scene.addChild(spriteNode)
         scene.scaleMode = .aspectFit
         
-        
-        
-       
-        
         skView.ignoresSiblingOrder = true
-        
-        
         skView.presentScene(scene)
-        
-        
     }
     
     
@@ -88,9 +110,65 @@ extension ViewController {
         return img
     }
     
- 
-     
-     
+    
+    
+    private func store(image: UIImage, forKey key: String, storageType: StorageType) {
+       
+        if let pngRepresentation = image.pngData() {
+           
+            switch storageType {
+            
+            case .fileSystem:
+                
+                if let filePath = filePath(forKey: key) {
+                    
+                    do  {
+                        try pngRepresentation.write(to: filePath, options: .atomic)
+                   
+                    } catch let error {
+                        print("ERROR SAVING FILE: ", error)
+                    }
+                }
+            
+            case .userDefaults:
+                UserDefaults.standard.set(pngRepresentation, forKey: key)
+            }
+        }
     }
+    
+    private func retrieveImage(forKey key: String, storageType: StorageType) -> UIImage? {
+       
+        switch storageType {
+        
+        case .fileSystem:
+            
+            if let filePath = self.filePath(forKey: key),
+                let fileData = FileManager.default.contents(atPath: filePath.path),
+                let image = UIImage(data: fileData) {
+                return image
+            }
+        
+        case .userDefaults:
+            
+            if let imageData = UserDefaults.standard.object(forKey: key) as? Data,
+                let image = UIImage(data: imageData) {
+                return image
+            }
+        }
+        
+        return nil
+    }
+    
+    
+    private func filePath(forKey key: String) -> URL?  {
+        let fileManager = FileManager.default
+        
+        guard let docURL = fileManager.urls(for: .documentDirectory,
+                                            in: FileManager.SearchPathDomainMask.userDomainMask).first else { return nil }
+        
+        return docURL.appendingPathComponent(key + ".png")
+    }
+    
+}
 
 
