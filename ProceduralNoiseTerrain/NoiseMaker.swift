276cc7f126fcdabe72e19fbfe228106b784e4b53
Andre Sarokhanian <andres1983@icloud.com>
2019-08-21 23:32:29 -0500

    minor tweaks

--- ProceduralNoiseTerrain/NoiseMaker.swift
+++ ProceduralNoiseTerrain/NoiseMaker.swift
@@ -31,36 +31,10 @@ class NoiseMaker {
       
         noiseSource.seed = Int32(arc4random_uniform(10000))
         let noise = GKNoise(noiseSource)
+        
         return noise
     }
     
-    static func perlinNoiseAnimated(_ sprite: SKSpriteNode) {
-        
-        let noiseSource = GKPerlinNoiseSource()
-        noiseSource.frequency = Double(arc4random_uniform(7) + 3)
-        noiseSource.octaveCount = Int(arc4random_uniform(7) + 4)
-        noiseSource.persistence = 0.5
-        noiseSource.lacunarity = 2.0
-        
-        noiseSource.seed = Int32(arc4random_uniform(10000))
-        let noise = GKNoise(noiseSource)
-        var map = GKNoiseMap(noise)
-        
-        let moveMap = SKAction.run {
-            map = GKNoiseMap(noise, size: vector2(2, 2),
-                                    origin: (vector2(map.origin.x+0.01, 0)),
-                                    sampleCount: vector2(300, 300),
-                                    seamless: false)
-            
-            sprite.texture = SKTexture(noiseMap: map)
-        }
-        
-        
-        let moveMapForever = SKAction.repeatForever(
-            (SKAction.sequence([moveMap,
-                                SKAction.wait(forDuration: 0.1)])))
-        sprite.run(moveMapForever)
-    }
    
     
     static func billowNoise() -> GKNoise {
