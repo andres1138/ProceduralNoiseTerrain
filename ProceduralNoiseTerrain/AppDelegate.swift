4c0fcc5aaf8455de3e33c5402d9314cd80c1d417
Andre Sarokhanian <andres1983@icloud.com>
2019-08-28 18:25:57 -0500

    organized files

--- ProceduralNoiseTerrain/AppDelegate.swift
+++ ProceduralNoiseTerrain/AppDelegate.swift
@@ -12,10 +12,22 @@ import UIKit
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
+    var coordinator: Coordinator?
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        
+        let navController = UINavigationController()
+        coordinator = MainCoordinator(navigationController: navController)
+        
+        coordinator?.startInitialViewController()
+        
+        window = UIWindow(frame: UIScreen.main.bounds)
+        window?.rootViewController = navController
+        window?.makeKeyAndVisible()
+        
+        
         return true
     }
 
